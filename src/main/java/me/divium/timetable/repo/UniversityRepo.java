9ecf96b8db35c15b29fe7643120cdce5cebf4810
Divium <79859179+Divium-l@users.noreply.github.com>
2023-10-01 21:31:20 +0300

    Added method to repo

--- src/main/java/me/divium/timetable/repo/UniversityRepo.java
+++ src/main/java/me/divium/timetable/repo/UniversityRepo.java
@@ -1,9 +1,14 @@
 package me.divium.timetable.repo;
 
 import me.divium.timetable.model.University;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 @Repository
 public interface UniversityRepo extends CrudRepository<University, Long> {
+    Optional<University> findByName(String name);
 }
