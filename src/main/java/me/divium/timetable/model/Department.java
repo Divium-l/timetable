9f1f002924b9d193c7261e2eb00c21666b50222a
Divium <79859179+Divium-l@users.noreply.github.com>
2023-10-01 21:30:38 +0300

    Create Department.java

---
+++ src/main/java/me/divium/timetable/model/Department.java
@@ -0,0 +1,20 @@
+package me.divium.timetable.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+
+@Data
+@Entity
+@RequiredArgsConstructor
+@Table(name = "departments")
+public class Department {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @NonNull
+    @Column(nullable = false, unique = true)
+    private String name;
+}
