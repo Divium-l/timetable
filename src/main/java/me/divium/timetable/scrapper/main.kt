9df52179fe7a7b018874a43c7117abeedea237c9
Divium <79859179+Divium-l@users.noreply.github.com>
2023-10-08 23:58:59 +0300

    added code for debugging

--- src/main/java/me/divium/timetable/scrapper/main.kt
+++ src/main/java/me/divium/timetable/scrapper/main.kt
@@ -4,18 +4,13 @@ import kotlinx.serialization.encodeToString
 import kotlinx.serialization.json.Json
 import me.divium.timetable.scrapper.exceptions.ParserException
 import me.divium.timetable.scrapper.model.timetable.SLessonInfo
+import me.divium.timetable.scrapper.scrappers.HtmlRutMobileGroupTimetableScrapper
 
 fun main() {
     try {
-//        val scrapper = HtmlRutMobileGroupTimetableScrapper("")
-//        scrapper.scrape()
-//        val timetable = scrapper.timetable
-//        val scrapper = HtmlRutDepartmentScrapper("https://miit.ru/timetable")
-//        scrapper.scrape()
-//        val result = scrapper.getResult()
-        val info = SLessonInfo("teacher name", "room number", "group name")
-        val json = Json.encodeToString(info)
-        println(json)
+        val scrapper = HtmlRutMobileGroupTimetableScrapper("https://miit.ru/timetable/193569")
+        scrapper.scrape()
+        val result = scrapper.getResult()
     } catch (e: ParserException) {
         println("Parsing failed with ${e.message}")
     }
